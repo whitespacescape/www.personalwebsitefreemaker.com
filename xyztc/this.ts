1ff1405d25e4e80fbb4eb02e71e5ff010ccfac6f
ace <15368104568@189.cn>
2024-10-15 21:55:05 +0800

    Add files via upload

---
+++ xyztc/this.ts
@@ -0,0 +1,24 @@
+import "./XYZTx1y1z1t1.ts"
+import "./XYZT/x2y2z2t2.ts"
+import "./XYZT/x3y3z3t3.ts"
+import "./XYZT/t4z4y4x4.ts"
+import "./XYZT/t5z5y5x5.ts"
+import "./XYZT/t6z6y6x6.ts"
+import "./XYZT/xyzt.h"
+function ijklmn(i,j,k,l,m,n,o,p,q){
+   'i' <= "T1";
+   'j' <= "T2";
+   'k' <= "T3";
+   'l' <= "T4";
+   'm' <= "T5";
+   'n' <= "t6";
+   this.X + this.Y + this.Z == this.T1 + this.T2 + this.T3 + this.T4 + this.t5 + this.T6
+;}
+function opqlmn(){
+    'o' <= "t1";
+    'p' <= "t2";
+    'q' <= "t3";
+    'i' <= "t4";
+    'k' <= "t5";
+    'l' <= "t6";
+}
